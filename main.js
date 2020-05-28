f56fd05cc180e9573365087e946a17dcf65b202e
Joey JEANTET <joey.jeantet@gmail.com>
2020-05-28 02:25:17 +0200

    Suppression des devTools

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function createWindow() {
         webPreferences: {
             contextIsolation: true,
             enableRemoteModule: false,
+            devTools: false,
             preload: path.join(__dirname + "/app", 'preload.js')
         }
     })
