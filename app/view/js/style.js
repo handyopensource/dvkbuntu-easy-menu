a766fad90f5d96fc58d126156f906401f5d27623
Joey JEANTET <joey.jeantet@gmail.com>
2020-05-29 14:57:03 +0200

    mise à jour graphique des boutons

--- app/view/js/style.js
+++ app/view/js/style.js
@@ -1,10 +1,27 @@
 window.api.getTheme()
     //fichier pour les paramètres de thèmes
 window.api.receive("setTheme", (style) => {
-    document.body.style.setProperty('--color-main', style["color-main"]);
-    document.body.style.setProperty('--color-secondary', style["color-secondary"]);
-    document.body.style.setProperty('--color-red', style["color-red"]);
-    document.body.style.setProperty('--color-gray', style["color-gray"]);
-    document.body.style.setProperty('--color-black', style["color-black"]);
-    document.body.style.setProperty('--color-text', style["color-text"]);
+    $(":root").css('--color-main', style["color-main"]);
+    $(":root").css('--color-secondary', style["color-secondary"]);
+    $(":root").css('--color-red', style["color-red"]);
+    $(":root").css('--color-gray', style["color-gray"]);
+    $(":root").css('--color-black', style["color-black"]);
+    $(":root").css('--color-text', style["color-text"]);
 })
+
+$(function() {
+    $('.tile').hover(function() {
+        $(this).find("div").removeClass("icon-wrapper");
+        $(this).find("div").addClass("icon-wrapper-selected");
+        $(this).find("span").removeClass("bottom-title")
+        $(this).find("span").addClass("bottom-title-selected")
+            //$(this).css("text-align", "center");
+    }, function() {
+        // on mouseout, reset the background colour
+        $(this).find("div").addClass("icon-wrapper");
+        $(this).find("div").removeClass("icon-wrapper-selected");
+        $(this).find("span").addClass("bottom-title")
+        $(this).find("span").removeClass("bottom-title-selected")
+            //$(this).css("text-align", "left");
+    });
+});
