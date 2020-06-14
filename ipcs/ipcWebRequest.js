7b93737be2c4ee1baf0242e0b6b221e016b389a7
Joey JEANTET <joey.jeantet@gmail.com>
2020-06-14 23:01:08 +0200

    Ajout du try catch sur le certificat

--- ipcs/ipcWebRequest.js
+++ ipcs/ipcWebRequest.js
@@ -1,16 +1,26 @@
 const { ipcMain } = require('electron')
 var Meta = require('html-metadata-parser');
+const Store = require('electron-store');
+const store = new Store();
 
 ipcMain.on('getWebsiteMeta', (event, data) => {
     (async() => {
-
-        var result = await Meta.parser(data.url);
-
-        event.sender.send('webMeta', {
-            id: data.id,
-            url: data.url,
-            result: result
-        })
+        try{
+            var result = await Meta.parser(data.url);
+            event.sender.send('webMeta', {
+                id: data.id,
+                url: data.url,
+                result: result
+            })
+        }catch(err){
+            console.log("Delete of website : "+data.url+" Certificate untrusted")
+            var toset = store.get('websites')
+            const index = toset.indexOf(data.url);
+            if (index > -1) {
+                toset.splice(index, 1);
+            }
+            store.set('websites', toset)
+        }
 
 
     })();
