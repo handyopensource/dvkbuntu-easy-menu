5ec45d2983f775ddf438f4b9297abce3f8e2860a
jeantet <joey.jeantet@gmail.com>
2020-06-24 17:49:35 +0000

    Modification du système de stockage de theme

--- ipcs/ipcStore.js
+++ ipcs/ipcStore.js
@@ -1,42 +1,61 @@
-const { app, ipcMain } = require('electron')
-const Store = require('electron-store');
-const path = require('path')
+const { app, ipcMain } = require("electron");
+const Store = require("electron-store");
+const path = require("path");
 
 const store = new Store();
 
-if(store.get('websites') == undefined){
-    store.set('websites', [])
+if (store.get("websites") == undefined) {
+  store.set("websites", []);
 }
 
 
+if (store.get("params") == undefined) {
+  store.set("params", { theme: "default" });
+  store.set("themes", {
+    default: {
+      "color-main": "#00316e",
+      "color-secondary": "#165786",
+      "color-gray": "#383838",
+      "color-black": "#0C0C0C",
+      "color-red": "#FF0000",
+      "color-text": "white",
+    }
+  });
+}
+
 function getDomainFromUrl(url) {
-    return url.replace("https://", "").split("/")[0]
+  return url.replace("https://", "").split("/")[0];
 }
 
 function buildDomainFromUrl(url) {
-    var str = getDomainFromUrl(url)
-    return "https://"+str
+  var str = getDomainFromUrl(url);
+  return "https://" + str;
 }
 
-ipcMain.on('getWebSiteStore', (event, data) => {
-    event.sender.send('getWebSiteStore', store.get('websites'))
-})
+ipcMain.on("getWebSiteStore", (event, data) => {
+  event.sender.send("getWebSiteStore", store.get("websites"));
+});
 
-ipcMain.on('setWebSiteStore', (event, data) => {
-    store.set('websites', data)
-})
+ipcMain.on("setWebSiteStore", (event, data) => {
+  store.set("websites", data);
+});
 
-ipcMain.on('addWebSiteStore', (event, data) => {
-    var toset = store.get('websites')
-    toset.push(buildDomainFromUrl(data))
-    store.set('websites', toset)
-})
+ipcMain.on("addWebSiteStore", (event, data) => {
+  var toset = store.get("websites");
+  toset.push(buildDomainFromUrl(data));
+  store.set("websites", toset);
+});
 
-ipcMain.on('remWebSiteStore', (event, data) => {
-    var toset = store.get('websites')
-    const index = toset.indexOf(data);
-    if (index > -1) {
-        toset.splice(index, 1);
-    }
-    store.set('websites', toset)
+ipcMain.on("remWebSiteStore", (event, data) => {
+  var toset = store.get("websites");
+  const index = toset.indexOf(data);
+  if (index > -1) {
+    toset.splice(index, 1);
+  }
+  store.set("websites", toset);
+});
+
+
+ipcMain.on("getTheme", (event) => {
+    event.sender.send('setTheme', store.get("themes")[store.get("params").theme])
 })
--- ipcs/ipcSystem.js
+++ ipcs/ipcSystem.js
@@ -6,16 +6,3 @@ const fs = require('fs')
 ipcMain.on('shutdown', () => {
     app.quit()
 })
-
-//evenement de requête de thème
-ipcMain.on("getTheme", (event, data) => {
-    var params = require("../data/params.json")
-    var path = "../data/theme/" + params.theme + ".json"
-    try {
-        var theme = require(path)
-        event.sender.send('setTheme', theme)
-    } catch (err) {
-        console.log("Error : no theme was found under the name of " + params.theme)
-    }
-
-})
